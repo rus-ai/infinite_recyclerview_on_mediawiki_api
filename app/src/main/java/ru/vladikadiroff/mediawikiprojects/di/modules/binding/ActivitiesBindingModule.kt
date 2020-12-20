eda511b9163327b384c1434546a54045fc79d808
vladi <vladikadiroff@gmail.com>
2020-12-20 20:20:48 +0300

    fix Scope for SplashActivity

--- app/src/main/java/ru/vladikadiroff/mediawikiprojects/di/modules/binding/ActivitiesBindingModule.kt
+++ app/src/main/java/ru/vladikadiroff/mediawikiprojects/di/modules/binding/ActivitiesBindingModule.kt
@@ -3,13 +3,14 @@ package ru.vladikadiroff.mediawikiprojects.di.modules.binding
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import ru.vladikadiroff.mediawikiprojects.di.scopes.ActivityScope
+import ru.vladikadiroff.mediawikiprojects.di.scopes.SplashScope
 import ru.vladikadiroff.mediawikiprojects.ui.activities.SplashActivity
 import ru.vladikadiroff.mediawikiprojects.ui.activities.WikiProjectsActivity
 
 @Module
 abstract class ActivitiesBindingModule {
 
-    @ActivityScope
+    @SplashScope
     @ContributesAndroidInjector
     abstract fun provideSplashActivity(): SplashActivity
 
